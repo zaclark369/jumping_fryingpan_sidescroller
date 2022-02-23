01459b9ef6f115244b84d4292d75f064026f059d
Zachary Clark <zacclark369@gmail.com>
2022-02-22 19:42:46 -0800

    finished javascript file, swapped pan for food

--- scripts.js
+++ scripts.js
@@ -1,8 +1,30 @@
-const fryingPan = document.getElementById('frying');
+const food = document.getElementById('frying');
 
 function jump() {
-    fryingPan.classList.add('jump');
+    if (food.classList != "jump") {
+    food.classList.add("jump");
+
+    setTimeout(function () {
+        food.classList.remove("jump");
+    }, 300);
 }
+}
+
+let isCooking = setInterval(function () {
+    // get the food y position
+    let foodTop = parseInt(window.getComputedStyle(food).getPropertyValue("top"));
+
+    // get the pan x position
+    let panLeft = parseInt(
+        window.getComputedStyle(pan).getPropertyValue('left')
+    );
+
+    // detect collisions
+    if (panLeft < 50 && panLeft > 0 && foodTop >= 140) {
+        // collision
+        alert("Your Cooked!");
+    }
+}, 10);
 
 
 document.addEventListener('keydown', function(event) {
