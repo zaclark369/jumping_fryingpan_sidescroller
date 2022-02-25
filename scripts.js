19b9023d174146d426beb00eceefe1026665386d
Zachary Clark <zacclark369@gmail.com>
2022-02-24 17:23:58 -0800

    added scoreing method and logic for random speeds of incoming object.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const food = document.getElementById('food');
 const pan = document.getElementById('pan');
+let score = 0;
 
 function jump() {
     if (food.classList != "jump") {
@@ -24,8 +25,23 @@ let isCooking = setInterval(function () {
     if (panLeft < 50 && panLeft > 0 && foodTop >= 140) {
         // collision
         alert("Your Cooked!");
+
+        // hide and show input for high score
+        score = 0;
+    } else if (panLeft < -20) {
+        score = score + 1;
+        console.log(score);
     }
 }, 10);
+
+let randomFunc = setInterval(function () {
+    max = 1500
+    min = 580
+    let randomNumber = Math.floor(Math.random() *(max-min))+min;
+    console.log(randomNumber);
+    pan.style.setProperty("--start_length",`${randomNumber}px`);
+    console.log(`${randomNumber}px`)
+}, 2000)
 
 
 document.addEventListener('keydown', function(event) {
